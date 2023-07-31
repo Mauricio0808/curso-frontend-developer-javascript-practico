02bc1242236b395bcfe0c89d5464648fef2c2e4f
Mauricio Cerquera <jomacedesarrollo@gmail.com>
2023-08-01 00:30:53 +0200

    fusion del carrito e interaccion del mismo con el menu mobile y desktop

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const burguerMenu = document.querySelector('.menu');
-const mobileMemu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
-burguerMenu.addEventListener('click',togglemobileMenu);
+burguerMenu.addEventListener('click',toggleMobileMenu);
+menuCarritoIcon.addEventListener('click',toggleCarritoAside)
 
 
 function toggleDesktopMenu() {
+   
     desktopMenu.classList.toggle('inactive');
+    aside.classList.add('inactive');
 }
 
-function togglemobileMenu() {
-    mobileMemu.classList.toggle('inactive');
+function toggleMobileMenu() {
+    
+    mobileMenu.classList.toggle('inactive')
+    aside.classList.add('inactive');
+
+}
+
+function toggleCarritoAside() {
+    
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+    aside.classList.toggle('inactive');
 }
