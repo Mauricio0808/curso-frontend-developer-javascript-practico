ce64a74576460a6f3d7c9346f67c90bcc62b01f3
Mauricio Cerquera <jomacedesarrollo@gmail.com>
2023-08-09 18:49:27 +0200

    incorporamos el product detail

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const burguerMenu = document.querySelector('.menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
 const mobileMenu = document.querySelector('.mobile-menu');
-const aside = document.querySelector('.product-detail')
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer')
 const cardsContainer = document.querySelector('.cards-container')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -14,13 +14,13 @@ menuCarritoIcon.addEventListener('click',toggleCarritoAside)
 function toggleDesktopMenu() {
    
     desktopMenu.classList.toggle('inactive');
-    aside.classList.add('inactive');
+    shoppingCartContainer.classList.add('inactive');
 }
 
 function toggleMobileMenu() {
     
     mobileMenu.classList.toggle('inactive')
-    aside.classList.add('inactive');
+    shoppingCartContainer.classList.add('inactive');
 
 }
 
@@ -28,7 +28,7 @@ function toggleCarritoAside() {
     
     desktopMenu.classList.add('inactive');
     mobileMenu.classList.add('inactive');
-    aside.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive');
 }
 
 const productList = [];
